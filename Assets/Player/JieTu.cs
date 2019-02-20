458800fcd29ac391450dd442d8f38ae6dceafbda
Alexandru Marin <marin.alexandru.andu@gmail.com>
2019-02-20 22:37:14 +0100

    Added 3d model and camera script.

---
+++ Assets/Player/JieTu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JieTu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public int jietuID = 0;
+	public string jietuname ;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			jietuID++;
+			jietuname ="screen_" +jietuID +".png";
+			ScreenCapture.CaptureScreenshot(jietuname,0);
+			print(jietuname);
+		}
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    private float offsetZ;
+    private float offsetY;
+
+    // Use this for initialization
+    void Start()
+    {
+        offsetY = this.transform.position.y;
+        offsetZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 destination = new Vector3();
+        destination.z = target.transform.position.z + offsetZ;
+        destination.x = target.transform.position.x;
+        destination.y = target.transform.position.y + offsetY;
+        this.transform.position = destination;
+    }
+}
